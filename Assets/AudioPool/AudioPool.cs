2e8ed0ef8471f9580f944695e2b40d1f8e7928ca
satanas <wil.alejandro@gmail.com>
2020-02-29 17:28:27 -0800

    Initial commit with project structure and audio pool class

---
+++ Assets/AudioPool/AudioPool.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+/*
+ * This class creates a pool of audio sources to play multiple audio clips simultaneously
+ */
+public class AudioPool : MonoBehaviour {
+    private const string ERROR_MESSAGE = "Sound dropped, no free channels available. Consider increasing the number of channels";
+    
+    public int channels;
+
+    private AudioSource[] sources;
+
+    void Start() {
+        sources = new AudioSource[channels];
+        for (int i = 0; i < sources.Length; i++) {
+            sources[i] = gameObject.AddComponent<AudioSource>();
+            sources[i].playOnAwake = false;
+            sources[i].loop = false;
+        }
+    }
+
+    public void PlayOneShot(AudioClip sound, float volume) {
+        AudioSource audioSrc = GetFreeChannel();
+        if (audioSrc != null) {
+            audioSrc.PlayOneShot(sound, volume);
+        } else {
+            Debug.Log(ERROR_MESSAGE);
+        }
+    }
+
+    public void PlayDelayed(AudioClip sound, float volume, float delay) {
+        AudioSource audioSrc = GetFreeChannel();
+        if (audioSrc != null) {
+            audioSrc.clip = sound;
+            audioSrc.volume = volume;
+            audioSrc.PlayDelayed(delay);
+        } else {
+            Debug.Log(ERROR_MESSAGE);
+        }
+    }
+
+    AudioSource GetFreeChannel() {
+        AudioSource rtn = null;
+
+        for (int i = 0; i < sources.Length; i++) {
+            if (!sources[i].isPlaying) {
+                rtn = sources[i];
+                break;
+            }
+        }
+
+        return rtn;
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# Unity Audio Pool
+
+## Installation
+
+## Usage
+
+## License
+
+AudioPool is licensed under the [Apache License, Version 2.0](https://www.apache.org/licenses/LICENSE-2.0).
+
+## Report issues
+
+## Donations
+
+AudioPool is free and open source because that is my way to contribute back to the gamedev community. However,
+if you love this project and want to show your appreciation, you can always donate to help me buy coffee that, of course,
+I later will turn into code ;)
+
+https://paypal.me/satanas82
